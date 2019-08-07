451d105124d27a9f8463ccca23c673f9af596b62
simoneas02 <simoneas02@hotmail.com>
2019-08-07 13:11:19 -0300

    create a Contact component

--- react-wdproject/src/components/App/index.js
+++ react-wdproject/src/components/App/index.js
@@ -3,12 +3,14 @@ import Header from '../Header';
 import Projects from '../Projects';
 
 import './app.css';
+import Contact from '../Contact';
 
 const App = () => (
   <div className="container">
     <Header />
     <main className="main">
       <Projects />
+      <Contact />
     </main>
   </div>
 );
---
+++ react-wdproject/src/components/Contact/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './contact.css';
+
+const Contact = () => (
+  <section class="contact">
+    <div class="contact-content">
+      <h2 class="contact__title">Let's Talk</h2>
+      <p class="contact__description">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod totam
+        consectetur fugiat nihil delectus, iste ipsa, recusandae, quis porro
+        doloribus natus veritatis quas. Animi nihil molestias eos doloribus ea.
+        Nemo!
+      </p>
+      <p class="contact__email">womendevprojects@gmail.com</p>
+    </div>
+  </section>
+);
+
+export default Contact;
